9d4cb74a8d0affc5f6295027dd3a489086b3e41e
polatbulut <polat.bulut1276@gmail.com>
2025-10-28 12:54:40 +0300

    docs: change config

--- src/preprocessing/config.py
+++ src/preprocessing/config.py
@@ -8,8 +8,8 @@ load_dotenv()
 class Env:
     openai_key: str = os.getenv("OPENAI_API_KEY", "")
     openai_base: str = os.getenv("OPENAI_BASE_URL", "https://api.openai.com/v1")
-    model: str = os.getenv("LABELICIOUS_MODEL", "gpt-5-mini")
-    temperature: float = float(os.getenv("LABELICIOUS_TEMPERATURE", 0.0))
-    top_p: float = float(os.getenv("LABELICIOUS_TOP_P", 1.0))
+    model: str = os.getenv("PREPROCESSING_MODEL", "gpt-5-mini")
+    temperature: float = float(os.getenv("PREPROCESSING_TEMPERATURE", 0.0))
+    top_p: float = float(os.getenv("PREPROCESSING_TOP_P", 1.0))
 
 ENV = Env()
